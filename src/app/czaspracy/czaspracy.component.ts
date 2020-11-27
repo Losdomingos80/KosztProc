5e78da6b4dd7cf793702b5998c52993878bf137d
Losdomingos80 <dominik@zparczewa.pl>
2020-11-27 12:47:42 +0100

    asas

--- src/app/czaspracy/czaspracy.component.ts
+++ src/app/czaspracy/czaspracy.component.ts
@@ -3,6 +3,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { MatDialog, MAT_DIALOG_DATA, MatDialogConfig } from '@angular/material/dialog';
 import {MatTableDataSource} from '@angular/material/table';
 
+import { CzaspracyService } from '../services/czaspracy.service';
+
 export interface DialogData {
   idProcedury: any;
   kodProcedury: any;
@@ -18,11 +20,23 @@ export interface DialogData {
 })
 export class CzaspracyComponent implements OnInit {
 
+  displayedColumns: string[] = ['rodzaj', 'akcje'];
   lista: any;
+  kosztyPracownicy: any;
+  kosztyPracownicyLista: any;
 
-  constructor(public dialogRef: MatDialogRef<CzaspracyComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+  constructor(public dialogRef: MatDialogRef<CzaspracyComponent>, 
+                    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+                    public pracownicy: CzaspracyService) { }
 
   ngOnInit(): void {
+    this.lista = false;
+    this.pobierzPraconikow();
+    this.pobierzKosztyPraconikow();
+    
+  }
+
+  off() {
     this.lista = false;
   }
   
@@ -33,4 +47,67 @@ export class CzaspracyComponent implements OnInit {
   add() {
     this.lista = true;
   }
+
+  addKoszt(id: any) {
+    console.log(id);
+    let ilosc: any;
+    ilosc = prompt("Podaj czas pracy w minutach");
+    console.log(ilosc);
+    this.pracownicy.addPracownicy(this.data.idProcedury, this.data.idOddzialu, id, ilosc)
+    .subscribe(response => {
+      this.lista = false;
+      this.pobierzPraconikow();
+      this.pobierzKosztyPraconikow();
+    });
+  }
+
+  pobierzPraconikow() {
+    this.pracownicy.getPracownicy(this.data.idProcedury, this.data.idOddzialu)
+      .subscribe(response => {
+        this.kosztyPracownicy = response;
+        console.log(this.kosztyPracownicy);
+      });
+    }
+  
+  pobierzKosztyPraconikow() {
+      this.pracownicy.getKosztyPracownicy(this.data.idProcedury, this.data.idOddzialu)
+        .subscribe(response => {
+          this.kosztyPracownicyLista = response;
+          console.log(this.kosztyPracownicyLista);
+        });
+      }
+
+
+  editKoszt(idPozycji: any, wartosc: any) {
+        console.log(wartosc);
+        let ilosc: any;
+        ilosc = prompt("Podaj nowy czas pracy w minutach", wartosc.toString());
+    
+        if(ilosc){
+          this.pracownicy.editKosztyPracownicy(idPozycji, ilosc)
+          .subscribe(response => {
+            this.pobierzPraconikow();
+            this.pobierzKosztyPraconikow();
+      
+          });
+        }
+    
+      }
+
+   delKoszt(idPozycji: any){
+
+        if (confirm("Czy napewno usunąć tą pozycję?")) {
+          this.pracownicy.delKosztyPracownicy(idPozycji)
+          .subscribe(response => {
+            this.pobierzPraconikow();
+            this.pobierzKosztyPraconikow();
+      
+          });
+          } else {
+          
+        } 
+       
+      }
+
+
 }
--- src/app/services/czaspracy.service.ts
+++ src/app/services/czaspracy.service.ts
@@ -7,11 +7,48 @@ import { HttpClient } from '@angular/common/http';
 export class CzaspracyService {
 
   private urlget = 'http://192.168.0.4/wycenyApi/czasPracownicy.php';
+  private urladd = 'http://192.168.0.4/wycenyApi/czasPraconicyadd.php';
+  private url = 'http://192.168.0.4/wycenyApi/czasKosztyPracownicy.php';
+  private urledit = 'http://192.168.0.4/wycenyApi/czasKosztyEdit.php';
+  private urldel = 'http://192.168.0.4/wycenyApi/czasKosztyDel.php'
 
   constructor(private httpClient: HttpClient) { }
 
-  getPracownicy(){
-        return this.httpClient.get(this.urlget);
-
+  getPracownicy(idProcedury: any, idOddzialu: any){
+        const postData = new FormData();
+        postData.append('idProcedury', idProcedury);
+        postData.append('idOddzialu', idOddzialu);
+        return this.httpClient.post(this.urlget, postData);
   }
+
+  addPracownicy(idProcedury: any, idOddzialu: any, idPracownika: any, ilosc: any){
+    const postData = new FormData();
+    postData.append('idProcedury', idProcedury);
+    postData.append('idOddzialu', idOddzialu);
+    postData.append('idPracownika', idPracownika);
+    postData.append('iloscJednostkowa', ilosc);
+    return this.httpClient.post(this.urladd, postData);
+  }
+
+  getKosztyPracownicy(idProcedury: any, idOddzialu: any){
+    const postData = new FormData();
+    postData.append('idProcedury', idProcedury);
+    postData.append('idOddzialu', idOddzialu);
+    return this.httpClient.post(this.url, postData);
+  }
+
+  editKosztyPracownicy(id: any, iloscJednostkowa: any){
+    const postData = new FormData();
+    postData.append('id', id);
+    postData.append('iloscJednostkowa', iloscJednostkowa);
+    return this.httpClient.post(this.urledit, postData);
+  }
+
+  delKosztyPracownicy(id: any){
+    const postData = new FormData();
+    postData.append('id', id);
+    return this.httpClient.post(this.urldel, postData);
+  }
+
 }
+                                                                               
